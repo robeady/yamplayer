614ff172899f736253effefaed195ae3aa5c04be
Rob Eady <robertj.eady@gmail.com>
2020-08-15 10:24:01 +0100

    reduce linter unhappiness

--- app/src/main.js
+++ app/src/main.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 const { app, BrowserWindow, session } = require("electron")
 const path = require("path")
 
@@ -42,7 +44,7 @@ app.on("activate", () => {
 // crazy hack to stop spew of console errors when using network devtools
 // see https://github.com/electron/electron/issues/13008#issuecomment-575909942
 app.on("ready", () => {
-    let redirectURL =
+    const redirectURL =
         "data:application/x-javascript;base64,UHJvZHVjdFJlZ2lzdHJ5SW1wbC5SZWdpc3RyeT1jbGFzc3tjb25zdHJ1Y3Rvcigpe31uYW1lRm9yVXJsKHIpe3JldHVybiBudWxsfWVudHJ5Rm9yVXJsKHIpe3JldHVybiBudWxsfXR5cGVGb3JVcmwocil7cmV0dXJuIG51bGx9fSxQcm9kdWN0UmVnaXN0cnlJbXBsLl9oYXNoRm9yRG9tYWluPWZ1bmN0aW9uKHIpe3JldHVybiIifSxQcm9kdWN0UmVnaXN0cnlJbXBsLnJlZ2lzdGVyPWZ1bmN0aW9uKHIsdCl7UHJvZHVjdFJlZ2lzdHJ5SW1wbC5fcHJvZHVjdHNCeURvbWFpbkhhc2g9bmV3IE1hcH0sUHJvZHVjdFJlZ2lzdHJ5SW1wbC5fcHJvZHVjdHNCeURvbWFpbkhhc2g9bmV3IE1hcCxQcm9kdWN0UmVnaXN0cnlJbXBsLnJlZ2lzdGVyKFtdLFtdKSxQcm9kdWN0UmVnaXN0cnlJbXBsLnNoYTE9ZnVuY3Rpb24ocil7cmV0dXJuIiJ9Ow=="
     session.defaultSession.webRequest.onBeforeRequest((details, callback) => {
         if (
