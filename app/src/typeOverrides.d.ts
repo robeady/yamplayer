c26779cdce03e3e65b4854249b0236c2a391de93
Rob Eady <robertj.eady@gmail.com>
2021-01-23 01:24:54 +0000

    upgrade linaria and babel

--- app/src/typeOverrides.d.ts
+++ app/src/typeOverrides.d.ts
@@ -1,17 +1,4 @@
-declare module "linaria" {
-    type CSSProperties = {
-        [key: string]: string | number | CSSProperties
-    }
-
-    export function css(
-        strings: TemplateStringsArray,
-        // template expression can also be a styled component
-        ...exprs: Array<string | number | CSSProperties | import("linaria/react").StyledComponent<unknown>>
-    ): string
-
-    export function cx(...classNames: Array<string | false | undefined | null | 0>): string
-}
-
+// completely forgot why we do this...
 declare module "*.svg" {
     const component: React.ComponentType<React.SVGProps<SVGSVGElement>>
     export default component
