eb0c733e27aaeba692ef3e84b8f8132d6882c79d
Rob Eady <robertj.eady@gmail.com>
2021-01-23 13:21:49 +0000

    box sizing border box

--- app/src/frontend/styles/global.ts
+++ app/src/frontend/styles/global.ts
@@ -6,5 +6,19 @@ export const globalCss = css`
             font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell,
                 "Helvetica Neue", sans-serif;
         }
+
+        html {
+            -webkit-box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            box-sizing: border-box;
+        }
+
+        *,
+        *::before,
+        *::after {
+            -webkit-box-sizing: inherit;
+            -moz-box-sizing: inherit;
+            box-sizing: inherit;
+        }
     }
 `
