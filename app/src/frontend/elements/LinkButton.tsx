5cad4b47a137ea04f52679cbb60e39bc04b46cd1
Rob Eady <robertj.eady@gmail.com>
2021-07-30 23:53:06 +0100

    style

--- app/src/frontend/elements/LinkButton.tsx
+++ app/src/frontend/elements/LinkButton.tsx
@@ -1,9 +1,11 @@
 import { styled } from "linaria/lib/react"
+import { colors } from "../styles"
 
 export const LinkButton = styled.button`
     border: 0;
     background: none;
     padding: 0;
+    color: ${colors.link};
     &:hover {
         text-decoration: underline;
         cursor: pointer;
