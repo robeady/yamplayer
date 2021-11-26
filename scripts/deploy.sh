e1de248c6577fb3769bc35fdf39a61ce3de3dcba
Rob Eady <robertj.eady@gmail.com>
2021-11-26 15:05:18 +0000

    error handling

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,36 +3,42 @@ set -euo pipefail
 
 function fileman_op {
     # destination is relative to parent directory of source
-    curl -H "Authorization: cpanel $USER:$TOKEN" \
-    "$HOST:2083/json-api/cpanel?cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=Fileman&cpanel_jsonapi_func=fileop&op=$1&sourcefiles=$2&destfiles=$3"
+    curl -s -H "Authorization: cpanel $USER:$TOKEN" \
+    "$HOST:2083/json-api/cpanel?cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=Fileman&cpanel_jsonapi_func=fileop&op=$1&sourcefiles=$2${3:+&destfiles=$3}" \
+    | ( ! jq -e ".cpanelresult.error" )
 }
 
 function fileman_upload {
-    curl -H "Authorization: cpanel $USER:$TOKEN" \
-    "$HOST:2083/json-api/cpanel?cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=Fileman&cpanel_jsonapi_func=uploadfiles" -F "file-=@$1" -F "dir=$2"
+    curl -s -H "Authorization: cpanel $USER:$TOKEN" \
+    "$HOST:2083/json-api/cpanel?cpanel_jsonapi_apiversion=2&cpanel_jsonapi_module=Fileman&cpanel_jsonapi_func=uploadfiles" -F "file-=@$1" -F "dir=$2" \
+    | ( ! jq -e ".cpanelresult.error" )
 }
 
-DATE=$(date --iso-8601 --utc)
+DATE=$(date -u +%FT%TZ)
 
-echo "reinstalling non-dev dependencies"
+echo "> reinstalling non-dev dependencies"
 mv node_modules node_modules_dev
 yarn install --frozen-lockfile --prod
 
-echo "zipping backend code and dependencies"
+echo "> zipping backend code and dependencies"
 zip -qr "$DATE.zip" dist node_modules -x "dist/frontend/*"
 
-echo "uploading zip file"
+echo "> uploading zip file"
 fileman_upload "$DATE.zip" yamplayer2
 
-echo "extracting zip file to new directory"
+echo "> extracting zip file to new directory"
 fileman_op extract "yamplayer2/$DATE.zip" "$DATE"
+fileman_op unlink "yamplayer2/$DATE.zip"
 
-echo "swapping old directory for new directory"
+echo "> swapping old directory for new directory"
 fileman_op move yamplayer2/live "pre-$DATE"
 fileman_op move "yamplayer2/$DATE" live
 
-echo "restarting app"
-touch restart.txt
-fileman_upload restart.txt yamplayer2/tmp
+echo "> restarting app"
+touch "restart_$DATE.txt"
+# upload cannot overwrite files
+fileman_upload "restart_$DATE.txt" yamplayer2/tmp
+fileman_op move "yamplayer2/tmp/restart_$DATE.txt" restart.txt
 
-echo "TODO: delete old directory"
+echo "> deleting old app directory"
+fileman_op unlink "yamplayer2/pre-$DATE"
