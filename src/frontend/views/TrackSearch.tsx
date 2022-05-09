ae49f618e4231a44314db76b9524fa21ae4971de
Rob Eady <robertj.eady@gmail.com>
2022-05-09 23:16:15 +0100

    write search query into url

--- src/frontend/views/TrackSearch.tsx
+++ src/frontend/views/TrackSearch.tsx
@@ -2,43 +2,42 @@ import { Stack, TextInput } from "@mantine/core"
 import React, { useEffect, useState } from "react"
 import { MdSearch } from "react-icons/md"
 import { useDispatch, useSelector } from "react-redux"
+import { useHistory, useLocation } from "react-router-dom"
 import { TrackListLegacy } from "../components/TrackListLegacy"
 import { catalogue } from "../state/actions"
 
 export function TrackSearch() {
     const dispatch = useDispatch()
-    const [searchQuery, setSearchQuery] = useState("")
-    const searchResults = useSelector(s =>
-        searchQuery ? s.catalogue.searchResultsByQuery[searchQuery] : undefined,
-    )
+
+    const history = useHistory()
+    const query = new URLSearchParams(useLocation().search).get("q")
+
+    const [text, setText] = useState<string>()
+
+    const searchResults = useSelector(s => (query ? s.catalogue.searchResultsByQuery[query] : undefined))
+
     useEffect(() => {
-        if (searchResults === undefined && searchQuery) {
-            void dispatch(catalogue.fetchSearchResults(searchQuery))
-        }
-    }, [dispatch, searchQuery, searchResults])
+        if (!searchResults && query) void dispatch(catalogue.fetchSearchResults(query))
+    }, [dispatch, query, searchResults])
     return (
         <Stack mt="md">
-            <SearchBox onSubmit={setSearchQuery} />
+            <TextInput
+                size="md"
+                placeholder="Search tracks"
+                icon={<MdSearch size={24} />}
+                sx={{ maxWidth: 500 }}
+                value={text ?? query ?? ""}
+                onChange={e => setText(e.target.value)}
+                onKeyPress={e => {
+                    if (e.key === "Enter" && text !== undefined) {
+                        history.replace({
+                            search: text ? "?" + new URLSearchParams({ q: text }).toString() : undefined,
+                        })
+                    }
+                }}
+            />
             <SearchResults trackIds={searchResults ? searchResults.externalTrackIds : []} />
         </Stack>
-    )
-}
-
-function SearchBox(props: { onSubmit: (text: string) => void }) {
-    const [text, setText] = useState("")
-
-    return (
-        <TextInput
-            size="md"
-            placeholder="Search tracks"
-            icon={<MdSearch size={24} />}
-            sx={{ maxWidth: 500 }}
-            value={text}
-            onChange={e => setText(e.target.value)}
-            onKeyPress={e => {
-                if (e.key === "Enter") props.onSubmit(text)
-            }}
-        />
     )
 }
 
