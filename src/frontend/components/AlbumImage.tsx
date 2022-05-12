89c081275993c6f9898eaae13cb2453f473d480f
Rob Eady <robertj.eady@gmail.com>
2022-05-13 00:46:51 +0100

    cleanup album image style

--- src/frontend/components/AlbumImage.tsx
+++ src/frontend/components/AlbumImage.tsx
@@ -1,22 +1,21 @@
-import { css, cx } from "linaria"
+import { styled } from "linaria/lib/react"
 import React from "react"
 import { Album } from "../../model"
 import { colors } from "../styles"
 
 export function AlbumImage(props: { className: string; album?: Album; size?: number }) {
     return (
-        <img
-            className={cx(
-                props.className,
-                css`
-                    border-radius: calc(4% + 2px);
-                    border: 1px solid ${colors.gray3};
-                    display: block;
-                `,
-            )}
+        <Img
+            className={props.className}
             src={props.album?.coverImageUrl ?? undefined}
             width={props.size}
             height={props.size}
         />
     )
 }
+
+const Img = styled.img`
+    border-radius: calc(4% + 2px);
+    border: 1px solid ${colors.gray3};
+    display: block;
+`
