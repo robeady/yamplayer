6253dd5d655d8cb29012876caeb1e4d98b0c375c
Rob Eady <robertj.eady@gmail.com>
2022-05-15 11:12:54 +0100

    artist image shadow

--- src/frontend/components/ArtistImage.tsx
+++ src/frontend/components/ArtistImage.tsx
@@ -16,4 +16,5 @@ export function ArtistImage(props: { className?: string; artist?: Artist; size?:
 const Img = styled.img`
     border-radius: 50%;
     display: block;
+    box-shadow: rgba(0, 0, 0, 0.1) 0 2px 12px;
 `
